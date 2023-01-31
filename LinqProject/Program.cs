18c0e0a00b10c652838de73b9f39078d356ba808
Onur ÖZDEMİR <ozdmronurr@gmail.com>
2023-01-31 13:09:42 +0300

    SQL-Linq

---
+++ LinqProject/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Net.Http.Headers;
+
+namespace LinqProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category> {
+            new Category { CategoryId =1,CategoryName="Bilgisayar"},
+            new Category { CategoryId =2,CategoryName="Telefon"}
+                        };
+            List<Product> products = new List<Product> {
+            new Product{ ProductId = 1,CategoryId=1,ProductName ="Acer Laptop",QuentityPerUnit ="32gb Ram",UnitsInStock =5,UnitPrice =10000},
+            new Product{ ProductId = 1,CategoryId=1,ProductName ="Asus Laptop",QuentityPerUnit ="16gb Ram",UnitsInStock =3,UnitPrice =8000},
+            new Product{ ProductId = 1,CategoryId=1,ProductName ="Hp Laptop",QuentityPerUnit ="8gb Ram",UnitsInStock =2,UnitPrice =6000},
+            new Product{ ProductId = 1,CategoryId=2,ProductName ="Samsung Telefon",QuentityPerUnit ="4 gb Ram",UnitsInStock =15,UnitPrice =5000},
+            new Product{ ProductId = 1,CategoryId=2,ProductName ="Apple Telefon",QuentityPerUnit ="4gb Ram",UnitsInStock =0,UnitPrice =8000},
+            };
+
+
+
+            Console.WriteLine("Linq-----------");
+
+
+          //  GetProductsLinq(products);
+            Console.WriteLine("Algoritmik-----------");
+
+           // GetProducts(products);
+            Console.ReadLine();
+        }
+
+        static List<Product> GetProducts(List<Product> products)
+        {      ///Linq olmasa yapılacaklar
+
+            List<Product> filteredProducts = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
+                {
+                    filteredProducts.Add(product);
+                }
+            }
+            return filteredProducts;
+
+        }
+        static List<Product> GetProductsLinq(List<Product> products)
+        {      //Linq Kullanımında
+
+            return products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3).ToList();
+        }
+
+    }
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuentityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,55 @@
+﻿--select
+select ContactName Adi,CompanyName Sirketadi,City Sehir from Customers 
+
+select * from Customers where City='Berlin'
+
+
+--case insensitive kucuk buyuk harf duyarlılığı yok select=SelEcT gibi  
+select * from Products where CategoryID='1' or CategoryID='3'
+
+select * from Products where CategoryID='1' and UnitPrice>=10
+
+select * from Products order by UnitPrice asc ----ascending artan ///// order by sıralama metodu
+
+select * from Products where  CategoryID ='1' order by UnitPrice desc ----descending düşen
+
+select Count(*) from Products --Kaçtane satır var
+
+select Count(*) from Products where CategoryID ='2'
+
+select Count(*) Adet from Products where CategoryID =2 -- katagoriId si 2 olanları say
+
+select CategoryID,count(*) Adet from Products group by  CategoryID  -- catagoriId'lerden hangisinden kaçtaane var
+
+select CategoryID,count(*) Adet from Products group by  CategoryID having count(*)<10 -- katagoriId'lerdekilerin katagorilerdekilerin 
+                                                                                      --10 teneden az olanları getir
+
+
+select CategoryID,count(*)  from Products where UnitPrice>20 group by  CategoryID having count(*)<10
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName 
+from Products inner join Categories on Products.CategoryID=Categories.CategoryID                 ----2 Tabloyu birleştirme join
+where Products.UnitPrice >10                                                                     ---- 2 tabloda eşleşenleri getirir.
+--DTO Data Transformation Object
+
+select * from Products p inner join [Order Details] od on p.ProductID = od.ProductID
+
+select * from Products p left join [Order Details] od on p.ProductID = od.ProductID  ---solda olup sağda olmayanları getirir
+
+select * from Products p right join [Order Details] od on p.ProductID = od.ProductID  ---- sağda olup solda olmyanları getirir
+
+select * from Customers c left join Orders o on c.CustomerID = o.CustomerID where o.CustomerID is null
+
+
+
+select * from Products p inner join [Order Details] od on p.ProductID = od.ProductID inner join Orders o on o.OrderID=od.OrderID
+
+
+Select p.ProductName 'Ürünün Adı', sum(od.UnitPrice*od.Quantity) 'Kazanılan Toplam Miktar'
+from Products p 
+inner join [Order Details] od on p.ProductID = od.ProductID
+inner join Orders o on od.OrderID = o.OrderID group by p.ProductName 
+
+Select p.ProductName 'Ürün Adı',SUM(od.UnitPrice*od.Quantity) 'Kazanılan Toplam Miktar'
+from Products p,[Order Details] od,Orders o where p.ProductID= od.ProductID and o.OrderID=od.OrderID
+group by p.ProductName
